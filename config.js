8bf602a27030603bfe6d37717e173873c3b72003
shishsu <vishnx09@gmail.com>
2026-02-01 20:45:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dcqhuseyo/video/upload/v1769958418/Engeyum_Kadhal_love_song_WhatsApp_status__shorts_whatsappstatus_love_soulbeatz_lovestatus_yt_youtube_MP3_160K_zhrofv.mp3 ", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
